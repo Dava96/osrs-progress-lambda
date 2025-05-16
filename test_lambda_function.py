6e628a46f8094d34dc63f2c24b62c1217d923c03
david.lomath-connis <david.lomath-connis@sykescottages.co.uk>
2025-05-16 15:36:36 +0100

    fix test

--- test_lambda_function.py
+++ test_lambda_function.py
@@ -1,10 +1,16 @@
+import unittest
 import json
 from lambda_function import lambda_handler
 
-def test_lambda_handler():
-    event = {}
-    context = None
-    response = lambda_handler(event, context)
-    assert response['statusCode'] == 200
-    body = json.loads(response['body'])
-    assert body['message'] == 'Hello from Lambda!'
+class TestLambdaHandler(unittest.TestCase):
+    def test_lambda_handler(self):
+        event = {}
+        context = None
+        response = lambda_handler(event, context)
+        self.assertEqual(response['statusCode'], 200)
+        body = json.loads(response['body'])
+        self.assertEqual(body['message'], 'Hello from Lambda!')
+
+
+if __name__ == '__main__':
+    unittest.main()
